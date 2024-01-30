ae940d00d85687624b8a37eeecf541350813a483
Ty Ha <hatranductai@gmail.com>
2024-01-30 17:16:37 -0600

    message

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    re_path(r"^(?!api/|admin/).*", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?!api/|admin|static/).*", TemplateView.as_view(template_name="index.html")),
 ]
