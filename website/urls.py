ae3c426f9ad2305c3bdbe4e2534f322b5f9bc19d
Ty Ha <hatranductai@gmail.com>
2024-01-30 17:20:49 -0600

    message

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     re_path(r"^(?!api/|admin|static/).*", TemplateView.as_view(template_name="index.html")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
