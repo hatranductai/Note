e2f5b52c5440b3c5250a65053cf509837f837fbc
Ty Ha <hatranductai@gmail.com>
2024-01-30 20:56:01 -0600

    message

--- website/settings.py
+++ website/settings.py
@@ -101,6 +101,6 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "build/static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
